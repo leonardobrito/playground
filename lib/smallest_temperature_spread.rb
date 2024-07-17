c3570fd2bb7dc18cdceb821ca009b869539edaaa
leonardobrito <f.leobrito@gmail.com>
2024-07-17 11:15:33 -0300

    Code refactor

--- lib/smallest_temperature_spread.rb
+++ lib/smallest_temperature_spread.rb
@@ -1,25 +1,11 @@
-require_relative "support/file_reader"
+require_relative "support/smallest_finder"
+
+def match_pattern(value)
+  value[0].match(/^(\d)+$/)
+end
 
 def smallest_temperature_spread
-  smallest_temperature_spread = Float::INFINITY
-  day_with_smallest_temperature_spread = nil
-
-  file_path = File.join(__dir__, 'assets', 'files', 'w_data.dat')
-
-  FileReader.retrieve_columns_from(file_path:, except_column_lambda: -> (columns) { columns[0].match(/^(\d)+$/) }) do |columns|
-    day = columns[0]
-    min_temperature = columns[2].to_i
-    max_temperature = columns[1].to_i
-
-    temperature_spread = max_temperature - min_temperature
-
-    if temperature_spread < smallest_temperature_spread
-      smallest_temperature_spread = temperature_spread
-      day_with_smallest_temperature_spread = day
-    end
-  end
-
-  day_with_smallest_temperature_spread
+  smallest_finder(0, 2, 1, "assets/files/w_data.dat", method(:match_pattern))
 end
 
 puts smallest_temperature_spread
---
+++ lib/support/smallest_finder.rb
@@ -0,0 +1,24 @@
+require_relative "file_reader"
+
+def smallest_finder(main_value_index, min_value_index, max_value_index, file_path_string, matcher_function)
+  smallest_value = Float::INFINITY
+  main_value = nil
+
+  file_path = File.join(__dir__, '..', file_path_string)
+
+  FileReader.retrieve_columns_from(file_path:, except_column_lambda: matcher_function) do |columns|
+    main_column_value = columns[main_value_index]
+    min_value = columns[min_value_index].to_i
+    max_value = columns[max_value_index].to_i
+
+    difference = max_value - min_value
+
+    if difference < smallest_value
+      smallest_value = difference
+      main_value = main_column_value
+    end
+
+  end
+
+  main_value
+end
